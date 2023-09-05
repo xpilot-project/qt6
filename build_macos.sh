23cabc8fcc567deaee82944352238a496f9f7dc9
Justin Shannon <justin@clowd.io>
2023-09-04 20:17:10 -0600

    Disable openssl linking on macOS

--- build_macos.sh
+++ build_macos.sh
@@ -5,8 +5,6 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-export PLATFORM="./dependencies/platform/macos"
-
 # Clone the Qt repository
 echo "Cloning the Qt repository..."
 git clone https://github.com/qt/qt5.git qt6 -b $1
@@ -22,36 +20,24 @@ perl init-repository --module-subset=qtbase,qtdeclarative,qtimageformats,qtmulti
 echo "Creating and entering the build directory..."
 mkdir -p build && cd build
 
-# Clone additional dependencies
-echo "Cloning additional dependencies..."
-git clone https://github.com/xpilot-project/dependencies.git
-
-# Set OPENSSL_LIBS environment variable
-echo "Setting OPENSSL_LIBS environment variable..."
-export OPENSSL_LIBS="$PLATFORM/openssl/libssl.a $PLATFORM/openssl/libcrypto.a"
-echo $OPENSSL_LIBS
-
-OPENSSL_ROOT_DIR="$(pwd)/dependencies/platform/macos/openssl"
-echo $OPENSSL_ROOT_DIR
-
 # Configure Qt build
 echo "Configuring Qt build..."
 ../configure -prefix ./install -debug-and-release -static -confirm-license \
-  -feature-relocatable -qt-zlib -nomake examples -nomake tests -no-dbus -openssl-linked \
+  -feature-relocatable -qt-zlib -nomake examples -nomake tests -no-dbus -no-openssl \
   -skip qt3d,qtactiveqt,qtandroidextras,qtcanvas3d,qtcharts,qtconnectivity \
   -skip qtdatavis3d,qtdoc,qtgamepad,qtgraphicaleffects,qtlocation,qtmacextras \
   -skip qtnetworkauth,qtpurchasing,qtremoteobjects,qtscript,qtscxml,qtsensors \
   -skip qtserialbus,qtspeech,qttools,qttranslations,qtvirtualkeyboard,qtwayland \
   -skip qtwebchannel,qtwebview,qtwinextras,qtx11extras,qtxmlpatterns,qtwebengine,qtimageformats \
-  -- -DOPENSSL_ROOT_DIR="$OPENSSL_ROOT_DIR" -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
+  -- -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
 
 # Build Qt
 echo "Building Qt..."
-cmake --build .
+cmake --build . 
 
+# Install Qt
 echo "Installing Qt..."
-cmake --install . --config Debug
-cmake --install . --config Release
+cmake --install .
 
 # Add files to 7z archive
 cd install && 7z a macos *
